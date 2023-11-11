eb9e70c0b59d130d76e4c2ca2e6335345d4b9c46
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-11-11 12:44:47 +0100

    zod functionality for login user

--- src/routes/authRoutes.routes.ts
+++ src/routes/authRoutes.routes.ts
@@ -3,12 +3,12 @@ import express from 'express'
 import { signup, login, verify } from '../controllers/auth.controllers'
 import { isAuthenticated } from '../middlewares/verifyToken.middleware'
 import { schemaValidation } from '../middlewares/schemaValidation.middleware'
-import { signUpSchema } from '../schemas/auth.schema'
+import { loginSchema, signUpSchema } from '../schemas/auth.schema'
 
 const router = express.Router()
 
 router.post('/signup', schemaValidation(signUpSchema), signup)
-router.post('/login', login)
+router.post('/login', schemaValidation(loginSchema), login)
 router.get('/verify', isAuthenticated, verify)
 
 export default router
