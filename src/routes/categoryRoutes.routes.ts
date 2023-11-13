634e25f5fccc1ef88603d0fd58b3dba92cf21656
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-11-13 15:00:04 +0100

    fixed routing for authentication and shema validation for updating category

--- src/routes/categoryRoutes.routes.ts
+++ src/routes/categoryRoutes.routes.ts
@@ -2,12 +2,13 @@
 import express from 'express'
 import { createCategory, getOneCategory, updateCategory } from '../controllers/category.controllers'
 import { schemaValidation } from '../middlewares/schemaValidation.middleware'
-import { createCategoryShema } from '../schemas/category.shema'
+import { createCategoryShema, updateCategorySchema } from '../schemas/category.shema'
+import { isAuthenticated } from '../middlewares/verifyToken.middleware'
 
 const router = express.Router()
 
-router.get('/getOneCategory/:categoryId', getOneCategory)
-router.post('/createCategory', schemaValidation(createCategoryShema), createCategory)
-router.put('/updateCategory/:categoryId', updateCategory)
+router.get('/getOneCategory/:categoryId', isAuthenticated, getOneCategory)
+router.post('/createCategory', isAuthenticated, schemaValidation(createCategoryShema), createCategory)
+router.put('/updateCategory/:categoryId', isAuthenticated, schemaValidation(updateCategorySchema), updateCategory)
 
 export default router
